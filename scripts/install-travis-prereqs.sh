9edf7740583b4782a41c1c3e4392d8f3fcb8f678
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-01-14 10:16:03 -0600

    update TeXLive dependencies
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- scripts/install-travis-prereqs.sh
+++ scripts/install-travis-prereqs.sh
@@ -6,4 +6,4 @@
 
 sudo apt-get update -qq
 
-sudo apt-get install -qq texlive
+sudo apt-get install -qq texlive help2man python-sphinx texlive-latex-base texlive-latex-recommended texlive-latex-extra texlive-fonts-recommended texinfo
